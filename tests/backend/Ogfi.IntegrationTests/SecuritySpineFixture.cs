7573240b7e40b823e490dcf7b8fe1a09dea61395
tuklasdigitallabs <tuklasdigitallabs@gmail.com>
2026-08-17 21:15:08 +0800

    fix(batch-b): import dependency injection extensions

--- tests/backend/Ogfi.IntegrationTests/SecuritySpineFixture.cs
+++ tests/backend/Ogfi.IntegrationTests/SecuritySpineFixture.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Npgsql;
 using Ogfi.Modules.Foundation.Persistence;
